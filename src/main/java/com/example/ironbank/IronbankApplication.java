17fc150e3386ea2fa5a25f1ccaff993cdf32edc6
acuencalara <acuencalara@gmail.com>
2022-10-15 10:46:56 +0200

    Initial commit

---
+++ src/main/java/com/example/ironbank/IronbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.ironbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ironbank/IronbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ironbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
