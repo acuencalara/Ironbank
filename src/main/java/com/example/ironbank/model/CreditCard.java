c2292c5d293981070838784ede99a29ff7ab2af8
acuencalara <acuencalara@gmail.com>
2022-10-26 11:54:03 +0200

    Updated Credit Card Account with Money Attribute

--- src/main/java/com/example/ironbank/model/CreditCard.java
+++ src/main/java/com/example/ironbank/model/CreditCard.java
@@ -3,7 +3,7 @@ package com.example.ironbank.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -13,6 +13,13 @@ import java.time.LocalDate;
 public class CreditCard extends BasicAccount{
 
     private BigDecimal interestRateCreditCard;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="amount",column =@Column(name="amountCreditLimit",insertable = false,updatable = false)),
+            @AttributeOverride(name="currency",column =@Column(name="currencyCreditLimit",insertable = false,updatable = false))
+    })
+    private Money creditLimit;
 
     public CreditCard(LocalDate creationDate, BigDecimal interestRateCreditCard) {
         super(creationDate);
